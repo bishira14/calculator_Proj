93296dc4be238e076cae76cf28c1322d769c3d52
Hamza Bishira <bishira12@hotmail.co.uk>
2020-02-28 15:41:57 +0000

    latest update

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 let theScreen = document.querySelector('.screen');
-let theButtons = document.querySelector('.btn');
+let theButtons = document.querySelectorAll('.btn');
+let equals = document.querySelector('.bttn-equals');
+let clear = document.querySelector('.bttn-clear');
+
+for(let i = 0; i < theButtons.length; i++){
+  theButtons[i].addEventListener('click', function(){
+    theScreen.value += theButtons[i].getAttribute('data-num');
+  })
+}
+
+equals.addEventListener('click', function(){
+  if(theScreen.value == ''){
+    alert('there is nothing to calculate')
+  }else{
+    let value = eval(theScreen.value);
+    theScreen.value = value;
+  }
+})
+
+clear.addEventListener('click', function(){
+  theScreen.value = '';
+})
